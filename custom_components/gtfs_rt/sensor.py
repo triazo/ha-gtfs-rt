3f486a18c60e6afaff5c63872ae28eee6dc88544
Abigail Freeman <abby@abby.cafe>
2024-03-18 18:53:19 -0400

    Simplify stuff a bit to get it to work.

--- custom_components/gtfs_rt/sensor.py
+++ custom_components/gtfs_rt/sensor.py
@@ -187,9 +187,7 @@ class PublicTransportData(object):
         for entity in feed.entity:
             if entity.HasField('trip_update'):
                 route_id = entity.trip_update.trip.route_id
-                direction_id = None
-                if 'direction_id' in entity.trip_update.trip:
-                    direction_id = entity.trip_update.trip.direction_id
+                direction_id = entity.trip_update.trip.direction_id
 
 
                 # Get link between vehicle_id from trip_id from vehicles positions if needed
